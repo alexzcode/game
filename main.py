a333316fb7c2403a45836e2ade0ef581a22fa862
Alexander Zjawin <alexander.zjawin@gmail.com>
2022-01-15 21:54:15 -0500

    Added Sky, Trees and grid placment

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e9985bad6e8a4bf2b471b7a6f8916e84a8070a5e058e4f551972f2b19027314
-size 829
+oid sha256:b8d2a8c1854c600cc9737d7fa37f719cf9c48d25a1aa62626c272f712ff68e29
+size 2111
